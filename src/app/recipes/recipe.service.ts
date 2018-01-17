a4b68dab1d62b77cb66e769fd11ad72116fc249e
bhagath <bhagath@getcustomexposure.com>
2018-01-18 01:30:49 +0530

    adding ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,20 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
-import { Recipe } from './recipe.model';
-
+import { Ingredient } from '../shared/ingredient.model';
 export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>();
 	
 	private recipes: Recipe[] = [
     new Recipe( 'A Test Recipe', 'This is simply a test', 'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg'),
-      new Recipe( 'Another Test Recipe', 'This is simply a test', 'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg')
+    [
+    	new Ingredient('Meat', 1),
+    	new Ingredient('French Fries', 20)
+    ],
+      new Recipe( 'Another Test Recipe', 'This is simply a test', 'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg',
+      [
+      	new Ingredient('Buns', 18),
+    	new Ingredient('Egg Roll', 30)
+      ])
   ];
 
   getRecipes() {
