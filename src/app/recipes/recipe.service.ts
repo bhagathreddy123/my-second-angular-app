f6d7a7b2db1c7596d9ae3cfe63419a73ebd885d0
bhagath <bhagath@getcustomexposure.com>
2018-01-19 18:28:24 +0530

    fixed syntax errors

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,12 +9,17 @@ export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>();
 	
 	private recipes: Recipe[] = [
-    new Recipe( 'A Test Recipe', 'This is simply a test', 'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg'),
+    new Recipe(
+     'A Test Recipe',
+      'This is simply a test',
+       'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg',
     [
     	new Ingredient('Meat', 1),
     	new Ingredient('French Fries', 20)
-    ],
-      new Recipe( 'Another Test Recipe', 'This is simply a test', 'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg',
+    ]),
+      new Recipe( 'Another Test Recipe', 
+      'This is simply a test',
+       'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg',
       [
       	new Ingredient('Buns', 18),
     	new Ingredient('Egg Roll', 30)
