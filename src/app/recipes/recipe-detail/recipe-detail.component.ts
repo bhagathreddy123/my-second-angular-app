7f014c829da8563df3e32779704157d5297feab9
bhagath <bhagath@getcustomexposure.com>
2018-01-20 21:35:54 +0530

    programtic navigation to edit page

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,9 +12,12 @@ import { ActivatedRoute } from '@angular/router';
 
 export class RecipeDetailComponent implements OnInit {
  recipe: Recipe;
+ id: number;
 
   constructor(private recipeService: RecipeService,
-  			private route: ActivatedRoute) { }
+  			     private route: ActivatedRoute,
+             private router: Router) {
+  }
 
   ngOnInit() {
 	this.route.params 
@@ -27,6 +31,11 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList() {
   	this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    //this.router.navigate(['edit'], {relativeTo: this.route});
+    this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route});
   }
 
 }
